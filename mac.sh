243a3db45f9e439b68a36c45deff3eb61532a36b
Peter <>
2024-06-16 21:53:24 +0300

    add macos setup script

---
+++ mac.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# Define ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Function to echo with yellow color and prefix
+print_yellow() {
+    local prefix_message="$1"
+    echo -e "${YELLOW}${prefix_message}${NC}"
+}
+
+# Function to echo with green color and prefix
+print_green() {
+    local prefix_message="$1"
+    echo -e "${GREEN}${prefix_message}${NC}"
+}
+
+# Function to echo with red color and prefix
+print_red() {
+    local prefix_message="$1"
+    echo -e "${RED}${prefix_message}${NC}"
+}
+
+# Function to check if a Homebrew package is installed
+is_installed() {
+    brew list "$1" &>/dev/null
+}
+
+# Function to prompt user for confirmation
+prompt_install() {
+    read -p "$(print_yellow "Do you want to install $1? (y/n): ")" choice
+    case "$choice" in
+        y|Y )
+            return 0;;
+        * )
+            return 1;;
+    esac
+}
+
+# Function to install an app depending on its type (core or cask)
+install_app() {
+    local app_type="$1"
+    local app_name="$2"
+
+    if is_installed "$app_name"; then
+        print_green "$app_name is already installed."
+        return
+    fi
+
+    if prompt_install "$app_name"; then
+        case "$app_type" in
+            core)
+                print_yellow "Installing: $app_name..."
+                brew install "$app_name"
+                ;;
+            cask)
+                print_yellow "Installing: $app_name..."
+                brew install --cask "$app_name"
+                ;;
+            *)
+                print_red "Error: Unsupported app type '$app_type'. Skipping installation of $app_name."
+                ;;
+        esac
+    else
+        print_yellow "Skipping installation of $app_name."
+    fi
+}
+
+# Main function to install all apps
+main() {
+    # List of core packages
+    local core_apps=(
+        "tmux"
+        "git"
+        "font-sf-pro"
+        "font-meslo-lg-nerd-font"
+        "powerlevel10k"
+        "zsh-syntax-highlighting"
+        "zsh-autosuggestions"
+    )
+
+    # List of cask packages
+    local cask_apps=(
+        "brave-browser"
+        "appcleaner"
+        "rectangle"
+        "wezterm"
+        "logi-options-plus"
+        "anki"
+        "telegram"
+        "calibre"
+        "obs"
+        "visual-studio-code"
+    )
+
+    print_yellow "Installing core packages..."
+    for app in "${core_apps[@]}"; do
+        install_app "core" "$app"
+    done
+
+    print_yellow "Installing cask packages..."
+    for app in "${cask_apps[@]}"; do
+        install_app "cask" "$app"
+    done
+}
+
+# Run the main function
+main
